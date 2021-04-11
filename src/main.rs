c951cd1b2e92213e2866b87e9fe62686b9e30548
Philip Munksgaard <philip@munksgaard.me>
2021-04-11 14:09:26 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use structopt::StructOpt;
+
+/// See the [structopt
+/// documentation](https://docs.rs/structopt/0.3.21/structopt) for more
+/// information.
+#[derive(StructOpt)]
+#[structopt(
+    name = "rust-template-project",
+    about = "A template you can use for your own projects."
+)]
+struct Args {}
+
+#[paw::main]
+fn main(_args: Args) -> Result<()> {
+    rust_template_project::my_library_function()
+}
