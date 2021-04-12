13cbaa1ea16389c966092c369dade29f2776f970
Philip Munksgaard <philip@munksgaard.me>
2021-04-12 11:37:08 +0200

    Add some test examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 pub fn my_library_function() -> Result<()> {
+    my_private_function(42)?;
     Ok(())
+}
+
+fn my_private_function(n: i32) -> Result<()> {
+    if n > 1000 {
+        bail!("`n` cannot be larger than 1000!")
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert!(my_private_function(42).is_ok());
+        assert!(my_private_function(9001).is_err());
+    }
 }
---
+++ tests/integration_test.rs
@@ -0,0 +1,6 @@
+use rust_template_project;
+
+#[test]
+fn it_returns_ok() {
+    assert!(rust_template_project::my_library_function().is_ok());
+}
