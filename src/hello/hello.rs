2cf95c5627a6105fa839a597a57494a88e10dc63
Thorkil Vaerge <thorkilk@gmail.com>
2021-04-13 15:53:00 +0200

    Init commit

---
+++ src/hello/hello.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // println! is a macro that prints to stdout
+    // Macros can be recognized by their names ending with an exclamation mark.
+    // Macros are expanded to ASTs and not through string preprocessing as in C,
+    // this avoids weird precedence behavior.
+    // `println!` is defined in the module std::fmt which contains utilities for
+    // formatting and printing strings.
+    println!("Hello World!");
+}
