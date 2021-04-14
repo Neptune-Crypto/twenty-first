c44db9c513332baa39afc49334ef0335043d8de8
Thorkil Vaerge <thorkilk@gmail.com>
2021-04-14 10:31:27 +0200

    Complete `tuples` tutorial

--- src/primitives/tuples.rs
+++ src/primitives/tuples.rs
@@ -1,4 +1,5 @@
 #![allow(unused_variables)]
+use std::fmt;
 
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     let (integer, boolean) = pair;
@@ -6,8 +7,25 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
     (boolean, integer)
 }
 
+fn transpose(input: Matrix) -> Matrix {
+    // First we destructure the tuple struct, this is done by declaring
+    // the value constructor on the LHS of the declaration equality sign
+    let Matrix(a, b, c, d) = input;
+    Matrix(a, c, b, d)
+}
+
+// printing and format string is handled by a series of macros in std::fmt
+// fmt::Debug uses the {:?} marker, formats text for debugging purposes, debug can be derived.
+// fmt::Display uses the {} marker, formats text in a more user friendly fashion, Display cannot be derived.
 #[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
+struct Matrix(f32, f32, f32, f32); // This is called a tuple struct
+
+// Implements fmt::Display for the Matrix datatype defined above
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
 
 pub fn tuples() {
     let long_tuple = (
@@ -51,59 +69,10 @@ pub fn tuples() {
     // Tuples can be destuctured to create bindings, just like in SML and in Haskell
     let tuple = (1, "hello", 4.5, true);
     let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix); // print using Debug
+    println!("Matrix: {}", matrix); // print using Display
+    println!("Transposed: {}", transpose(matrix)); // print using Display
 }
-
-// // Tuples can be used as function arguments and as return values
-// fn reverse(pair: (i32, bool)) -> (bool, i32) {
-//     // `let` can be used to bind the members of a tuple to variables
-//     let (integer, boolean) = pair;
-
-//     (boolean, integer)
-// }
-
-// // The following struct is for the activity.
-// #[derive(Debug)]
-// struct Matrix(f32, f32, f32, f32);
-
-// fn main() {
-//     // A tuple with a bunch of different types
-//     let long_tuple = (1u8, 2u16, 3u32, 4u64,
-//                       -1i8, -2i16, -3i32, -4i64,
-//                       0.1f32, 0.2f64,
-//                       'a', true);
-
-//     // Values can be extracted from the tuple using tuple indexing
-//     println!("long tuple first value: {}", long_tuple.0);
-//     println!("long tuple second value: {}", long_tuple.1);
-
-//     // Tuples can be tuple members
-//     let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
-
-//     // Tuples are printable
-//     println!("tuple of tuples: {:?}", tuple_of_tuples);
-
-//     // But long Tuples cannot be printed
-//     // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-//     // println!("too long tuple: {:?}", too_long_tuple);
-//     // TODO ^ Uncomment the above 2 lines to see the compiler error
-
-//     let pair = (1, true);
-//     println!("pair is {:?}", pair);
-
-//     println!("the reversed pair is {:?}", reverse(pair));
-
-//     // To create one element tuples, the comma is required to tell them apart
-//     // from a literal surrounded by parentheses
-//     println!("one element tuple: {:?}", (5u32,));
-//     println!("just an integer: {:?}", (5u32));
-
-//     //tuples can be destructured to create bindings
-//     let tuple = (1, "hello", 4.5, true);
-
-//     let (a, b, c, d) = tuple;
-//     println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
-
-//     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-//     println!("{:?}", matrix);
-
-// }
