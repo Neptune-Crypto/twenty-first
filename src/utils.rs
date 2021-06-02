e998171ef330625d51ea2ca0848cb25345219b41
Thorkil Vaerge <thorkilk@gmail.com>
2021-06-02 22:49:21 +0200

    Move rng helper functions to utils
    Note that this helper function currently only generates values up from 0
    to 255. This should be fixed.

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use rand::RngCore;
 use std::collections::HashSet;
 use std::hash::Hash;
 
@@ -10,6 +11,18 @@ where
 {
     let mut uniq = HashSet::new();
     iter.into_iter().all(move |x| uniq.insert(x))
+}
+
+pub fn generate_random_numbers(size: usize, modulus: i128) -> Vec<i128> {
+    let mut prng = rand::thread_rng();
+    let mut rand = vec![0u8; size * 32];
+    prng.fill_bytes(rand.as_mut_slice());
+
+    // This looks pretty inefficient
+    // How is this done with a map instead?
+    // TODO: Only generates values up to 256!
+    let values: Vec<i128> = rand.iter().map(|&x| x as i128 % modulus).collect();
+    values
 }
 
 #[cfg(test)]
