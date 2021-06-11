52f66f1fe501fe24c32df4a868704269f6dad851
Thor <Thorkil Vrge Sword-Smith>
2021-06-11 18:38:20 +0200

    WIP5 -- seems to be working with compressed proofs

--- src/util_types/merkle_tree_vector.rs
+++ src/util_types/merkle_tree_vector.rs
@@ -1,8 +1,10 @@
 use crate::shared_math::other::log_2;
 use serde::Serialize;
+use std::cmp::Reverse;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Node<T> {
     value: Option<T>,
     hash: [u8; 32],
@@ -38,6 +40,8 @@ impl<T: Clone + Serialize + Debug + PartialEq> MerkleTreeVector<T> {
                 .as_slice(),
         )
         .as_bytes();
+        println!("root_hash = {:?}", root_hash);
+        println!("v.hash = {:?}", v.hash);
         v.hash == root_hash && expected_hash == proof[0].hash
     }
 
@@ -88,11 +92,140 @@ impl<T: Clone + Serialize + Debug + PartialEq> MerkleTreeVector<T> {
         proof
     }
 
-    // pub fn get_multi_proof(&self, indices: Vec<usize>) -> Vec<Vec<Node<T>>> {
-    //     let output =
+    pub fn verify_multi_proof(
+        root_hash: [u8; 32],
+        indices: Vec<usize>,
+        proof: Vec<Vec<Option<Node<T>>>>,
+    ) -> bool {
+        let mut partial_tree: HashMap<u64, Node<T>> = HashMap::new();
+        let mut proof_clone = proof.clone();
+        let half_tree_size = 2u64.pow(proof_clone[0].len() as u32 - 1);
+        for (i, b) in indices.iter().zip(proof_clone.iter_mut()) {
+            let mut index = half_tree_size + *i as u64;
+            partial_tree.insert(index, b[0].clone().unwrap());
+            for j in 1..b.len() {
+                if let Some(i) = b[j].clone() {
+                    partial_tree.insert(index ^ 1, i);
+                }
+                index /= 2;
+            }
+        }
 
-    //     vec![]
-    // }
+        let mut complete = false;
+        let mut hasher = blake3::Hasher::new();
+        while !complete {
+            complete = true;
+            //let mut keys: Vec<usize> = partial_tree.iter().copied().map(|x| x / 2).collect();
+            let mut keys: Vec<u64> = partial_tree.keys().copied().map(|x| x / 2).collect();
+            keys.sort_by_key(|w| Reverse(*w));
+            for key in keys {
+                if partial_tree.contains_key(&(key * 2))
+                    && partial_tree.contains_key(&(key * 2 + 1))
+                    && !partial_tree.contains_key(&key)
+                {
+                    hasher.update(&partial_tree[&(key * 2)].hash[..]);
+                    hasher.update(&partial_tree[&(key * 2 + 1)].hash[..]);
+                    partial_tree.insert(
+                        key,
+                        Node {
+                            value: None,
+                            hash: *hasher.finalize().as_bytes(),
+                        },
+                    );
+                    hasher.reset();
+                    complete = false;
+                }
+            }
+        }
+
+        for (i, b) in indices.iter().zip(proof_clone.iter_mut()) {
+            let mut index = half_tree_size + *i as u64;
+            for j in 1..b.len() {
+                if b[j] == None {
+                    b[j] = Some(partial_tree[&(index ^ 1)].clone());
+                }
+                partial_tree.insert(index ^ 1, b[j].clone().unwrap());
+                index /= 2;
+            }
+        }
+
+        // let proof_clone_unwrapped: Vec<Vec<Node<T>>> = proof_clone
+        //     .clone()
+        //     .into_iter()
+        //     .map(|x| x.into_iter().map(|y| y.unwrap()))
+        //     .into_iter()
+        //     //.map(|x| x.unwrap())
+        //     .collect();
+        for i in 0..indices.len() {
+            let proof_clone_unwrapped: Vec<Node<T>> = proof_clone[i]
+                .clone()
+                .into_iter()
+                .map(|x| x.unwrap())
+                .collect();
+            println!("input_proof = {:?}", proof[i]);
+            println!("proof_clone_unwrapped = {:?}", proof_clone_unwrapped);
+            if !Self::verify_proof(root_hash, indices[i] as u64, proof_clone_unwrapped) {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn get_multi_proof(&self, indices: Vec<usize>) -> Vec<Vec<Option<Node<T>>>> {
+        let mut calculable_indices: HashSet<usize> = HashSet::new();
+        let mut output: Vec<Vec<Option<Node<T>>>> = Vec::with_capacity(indices.len());
+        for i in indices.iter() {
+            let new_branch: Vec<Option<Node<T>>> =
+                self.get_proof(*i).into_iter().map(Some).collect();
+            let mut index = self.nodes.len() / 2 + i;
+            calculable_indices.insert(index);
+            for _ in 1..new_branch.len() {
+                calculable_indices.insert(index ^ 1);
+                index /= 2;
+            }
+            output.push(new_branch);
+        }
+
+        let mut complete = false;
+        while !complete {
+            complete = true;
+            let mut keys: Vec<usize> = calculable_indices.iter().copied().map(|x| x / 2).collect();
+            // reverse sort, from big to small, This should be the fastest way to reverse sort.
+            // cf. https://stackoverflow.com/a/60916195/2574407
+            keys.sort_by_key(|w| Reverse(*w));
+            for key in keys.iter() {
+                if calculable_indices.contains(&(key * 2))
+                    && calculable_indices.contains(&(key * 2 + 1))
+                    && !calculable_indices.contains(key)
+                {
+                    calculable_indices.insert(*key);
+                    complete = false;
+                }
+            }
+        }
+
+        let mut scanned: HashSet<usize> = HashSet::new();
+        for (i, b) in indices.iter().zip(output.iter_mut()) {
+            let mut index: usize = self.nodes.len() / 2 + i;
+            scanned.insert(index);
+            // let mut remove: HashSet<usize> = HashSet::new();
+            for j in 1..b.len() {
+                if calculable_indices.contains(&((index ^ 1) * 2))
+                    && calculable_indices.contains(&((index ^ 1) * 2 + 1))
+                    || (index ^ 1) as i64 - self.nodes.len() as i64 / 2 > 0 // TODO: Maybe > 1 here?
+                        && indices.contains(&((index ^ 1) - self.nodes.len() / 2))
+                    || scanned.contains(&(index ^ 1))
+                {
+                    b[j] = None;
+                    // b[j].hash = [0u8; 32]; // TODO: Remove value instead
+                }
+                scanned.insert(index ^ 1);
+                index /= 2;
+            }
+        }
+
+        output
+    }
 }
 
 #[cfg(test)]
@@ -184,6 +317,7 @@ mod merkle_tree_vector_test {
             1,
             proof.clone()
         ));
+        let original_root = mt_four.root_hash.clone();
         mt_four.root_hash = [0u8; 32];
         assert!(!MerkleTreeVector::verify_proof(
             mt_four.root_hash,
@@ -191,5 +325,42 @@ mod merkle_tree_vector_test {
             proof.clone()
         ));
         println!("get_proof(mt_four) = {:x?}", proof);
+        mt_four.root_hash = original_root;
+
+        println!("root_hash = {:?}", mt_four.root_hash);
+        proof = mt_four.get_proof(0);
+        println!("root_hash = {:?}", mt_four.root_hash);
+        println!("\n\n\n\n proof(0) = {:?} \n\n\n\n", proof);
+        assert!(MerkleTreeVector::verify_proof(mt_four.root_hash, 0, proof));
+        let mut compressed_proof = mt_four.get_multi_proof(vec![0]);
+        assert!(MerkleTreeVector::verify_multi_proof(
+            mt_four.root_hash,
+            vec![0],
+            compressed_proof.clone()
+        ));
+        proof = mt_four.get_proof(0);
+        assert_eq!(proof.len(), compressed_proof[0].len());
+        let unwrapped_compressed_proof: Vec<Node<i128>> = compressed_proof[0]
+            .clone()
+            .into_iter()
+            .map(|x| x.unwrap())
+            .collect();
+        assert_eq!(proof, unwrapped_compressed_proof);
+        println!("{:?}", compressed_proof);
+
+        compressed_proof = mt_four.get_multi_proof(vec![0, 1]);
+        println!("{:?}", compressed_proof);
+        assert!(MerkleTreeVector::verify_multi_proof(
+            mt_four.root_hash,
+            vec![0, 1],
+            compressed_proof
+        ));
+        compressed_proof = mt_four.get_multi_proof(vec![0, 1, 2]);
+        println!("{:?}", compressed_proof);
+        assert!(MerkleTreeVector::verify_multi_proof(
+            mt_four.root_hash,
+            vec![0, 1, 2],
+            compressed_proof
+        ));
     }
 }
