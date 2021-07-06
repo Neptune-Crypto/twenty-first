05a1b3f5a4d9d6f977657553aab52d146738e755
Thor <Thorkil Vrge Sword-Smith>
2021-07-07 00:29:39 +0200

    Add PrimeFieldElement struct for bigints
    This new struct adds support for arbitrary size prime field elements.

--- src/shared_math.rs
+++ src/shared_math.rs
@@ -6,6 +6,7 @@ pub mod other;
 pub mod polynomial;
 pub mod polynomial_quotient_ring;
 pub mod prime_field_element;
+pub mod prime_field_element_big;
 pub mod prime_field_polynomial;
 pub mod stark;
 pub mod traits;
