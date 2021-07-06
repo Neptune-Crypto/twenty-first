effb36e07beeddd3cc200180b25fe74a5fd65dc4
Thorkil Vaerge <thorkilk@gmail.com>
2021-07-06 16:37:58 +0200

    Add nice traits for field elements

--- src/shared_math/traits.rs
+++ src/shared_math/traits.rs
@@ -1,6 +1,42 @@
+use std::convert::{From, TryInto};
+use std::fmt::{Debug, Display};
+use std::hash::Hash;
+
 pub trait IdentityValues {
     fn is_zero(&self) -> bool;
     fn is_one(&self) -> bool;
     fn ring_zero(&self) -> Self;
     fn ring_one(&self) -> Self;
+}
+
+pub trait FieldElement:
+    num_traits::Num
+    + Clone
+    + Hash
+    + Debug
+    + Display
+    + PartialEq
+    + Eq
+    + PartialOrd
+    + Ord
+    + From<i128>
+    + TryInto<i128>
+{
+    fn is_power_of_2(&self) -> bool;
+}
+impl<
+        T: num_traits::Num
+            + Clone
+            + Copy
+            + Hash
+            + Debug
+            + Display
+            + PartialEq
+            + Eq
+            + PartialOrd
+            + Ord
+            + From<i128>
+            + TryInto<i128>,
+    > FieldElement for T
+{
 }
