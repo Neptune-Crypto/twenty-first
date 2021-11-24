cb9a0d19b51a1030ed663703cc22b602f0d6de57
sword-smith <thorkilk@gmail.com>
2021-11-24 16:00:07 +0100

    Complete Rescue Prime STARK signature scheme
    Very similar to the one in Alan's tutorial, the slow version. The next
    steps would be to make this code fast. Current runtimes, as achieved in
    the `test_rpsss::sign_verify_test` (thus not production (fast)
    compiled):
    Proof time: 11.609640847s
    Verification time: 2.628209281s
    
    Proof time: 10.788729567s
    Verification time: 2.484401581s
    
    Proof time: 10.513710249s
    Verification time: 2.436293808s
    
    We should be able to lower at least the proof time.

--- src/shared_math/rpsss.rs
+++ src/shared_math/rpsss.rs
@@ -2,8 +2,11 @@ use super::prime_field_element_big::{PrimeFieldBig, PrimeFieldElementBig};
 use super::stark::Stark;
 use crate::shared_math::rescue_prime_stark::RescuePrime;
 use crate::util_types::proof_stream::ProofStream;
+use crate::utils::blake3_digest;
 use rand::RngCore;
 use std::error::Error;
+
+const DOCUMENT_HASH_LENGTH: usize = 32usize;
 
 #[derive(Clone, Debug)]
 pub struct SecretKey<'a> {
@@ -40,10 +43,31 @@ impl<'a> RPSSS<'a> {
         (sk, pk)
     }
 
-    pub fn sign(&self, sk: SecretKey, document: Vec<u8>) -> Result<Signature, Box<dyn Error>> {
+    pub fn verify(&self, public_key: &PublicKey, signature: &Signature, document: &[u8]) -> bool {
+        // Verify that the signature is prepended with the hash of the document
+        let document_hash = blake3_digest(document);
+        if signature.proof[0..DOCUMENT_HASH_LENGTH] != document_hash {
+            return false;
+        }
+
+        let mut proof_stream: ProofStream = signature.proof.clone().into();
+        proof_stream.set_index(DOCUMENT_HASH_LENGTH);
+
+        let boundary_constraints = self.rp.get_boundary_constraints(&public_key.value);
+        let transition_constraints = self.rp.get_air_constraints(&self.stark.omicron);
+        let res = self.stark.verify(
+            &mut proof_stream,
+            transition_constraints,
+            boundary_constraints,
+        );
+
+        res.is_ok()
+    }
+
+    pub fn sign(&self, sk: &SecretKey, document: &[u8]) -> Result<Signature, Box<dyn Error>> {
         let (output, trace) = self.rp.eval_and_trace(&sk.value);
-        // let mut proof_stream = ProofStream::signature_proof_stream(document);
-        let mut proof_stream = ProofStream::new_with_prefix(&document);
+        let document_hash = blake3_digest(document);
+        let mut proof_stream = ProofStream::new_with_prefix(&document_hash);
         self.stark.prove(
             trace,
             self.rp.get_air_constraints(&self.stark.omicron),
@@ -54,5 +78,66 @@ impl<'a> RPSSS<'a> {
         Ok(Signature {
             proof: proof_stream.serialize(),
         })
+    }
+}
+
+#[cfg(test)]
+mod test_rpsss {
+    use std::time::Instant;
+
+    use super::super::stark::test_stark;
+    use super::*;
+    use crate::shared_math::prime_field_element_big::PrimeFieldBig;
+    use num_bigint::BigInt;
+
+    #[test]
+    fn sign_verify_test() {
+        let modulus: BigInt = (407u128 * (1 << 119) + 1).into();
+        let field = PrimeFieldBig::new(modulus);
+        let (stark, rp) = test_stark::get_tutorial_stark(&field);
+        let rpsss = RPSSS {
+            field: field.clone(),
+            stark,
+            rp,
+        };
+        let document_string: String = "Hello Neptune!".to_string();
+        let document: Vec<u8> = document_string.clone().into_bytes();
+
+        let (sk, pk) = rpsss.keygen();
+        println!("secret key = {}, public key = {}", sk.value, pk.value);
+        let mut start = Instant::now();
+        let mut signature: Signature = rpsss.sign(&sk, &document).unwrap();
+        let proof_duration = start.elapsed();
+        start = Instant::now();
+        assert!(rpsss.verify(&pk, &signature, &document));
+        let verify_duration = start.elapsed();
+        println!("Proof time: {:?}", proof_duration);
+        println!("Verification time: {:?}", verify_duration);
+
+        if let Some(last) = signature.proof.last_mut() {
+            *last = *last ^ 0x01;
+        }
+
+        assert!(!rpsss.verify(&pk, &signature, &document));
+
+        // Set the last byte of the proof stream back and change the
+        // first byte and verify that it still fails
+        if let Some(last) = signature.proof.last_mut() {
+            *last = *last ^ 0x01;
+        }
+        if let Some(first) = signature.proof.first_mut() {
+            *first = *first ^ 0x01;
+        }
+
+        assert!(!rpsss.verify(&pk, &signature, &document));
+
+        // A valid proof is rejected if the `document` argument is wrong
+        if let Some(first) = signature.proof.first_mut() {
+            *first = *first ^ 0x01;
+        }
+        let bad_document_string: String = "Hello Saturn!".to_string();
+        let bad_document: Vec<u8> = bad_document_string.clone().into_bytes();
+        assert!(!rpsss.verify(&pk, &signature, &bad_document));
+        assert!(rpsss.verify(&pk, &signature, &document));
     }
 }
