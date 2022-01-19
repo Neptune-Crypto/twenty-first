ef48cd4cfd5f7c7df312dda08a07826d98a6f34c
Simon Shine <simon@neptune.cash>
2022-01-19 15:30:32 +0100

    Move rp.eval_and_trace(), get_primitive_root_of_unity() and rp.get_air_constraints() out of benchmark

--- benches/stark.rs
+++ benches/stark.rs
@@ -12,28 +12,29 @@ fn stark_medium(criterion: &mut Criterion) {
     let rp: RescuePrime = params::rescue_prime_medium_test_params();
     let stark: Stark = Stark::new(16, 2, rp.m as u32, BFieldElement::new(7));
 
+    let mut timer = TimingReporter::start();
+
+    let one = BFieldElement::ring_one();
+    let (output, trace) = rp.eval_and_trace(&one);
+    timer.elapsed("rp.eval_and_trace(...)");
+    let omicron = BFieldElement::ring_zero()
+        .get_primitive_root_of_unity(16)
+        .0
+        .unwrap();
+    timer.elapsed("BFieldElement::get_primitive_root_of_unity(16)");
+    let air_constraints = rp.get_air_constraints(omicron);
+    timer.elapsed("rp.get_air_constraints(omicron)");
+    let boundary_constraints = rp.get_boundary_constraints(output);
+    timer.elapsed("rp.get_boundary_constraints(...)");
+
+    let report = timer.finish();
+    println!("{}", report);
+
     let mut group = criterion.benchmark_group("stark");
     group.sample_size(10);
-    let benchmark_id = BenchmarkId::new("medium", 5);
+    let benchmark_id = BenchmarkId::new("large", 5);
     group.bench_function(benchmark_id, |bencher| {
         bencher.iter(|| {
-            let mut timer = TimingReporter::start();
-
-            let one = BFieldElement::ring_one();
-            let (output, trace) = rp.eval_and_trace(&one);
-            timer.elapsed("rp.eval_and_trace(...)");
-            let omicron = BFieldElement::ring_zero()
-                .get_primitive_root_of_unity(16)
-                .0
-                .unwrap();
-            timer.elapsed("BFieldElement::get_primitive_root_of_unity(16)");
-            let air_constraints = rp.get_air_constraints(omicron);
-            timer.elapsed("rp.get_air_constraints(omicron)");
-            let boundary_constraints = rp.get_boundary_constraints(output);
-            timer.elapsed("rp.get_boundary_constraints(...)");
-            let report = timer.finish();
-            println!("{}", report);
-
             let mut proof_stream = ProofStream::default();
 
             let prove_result = stark.prove(
