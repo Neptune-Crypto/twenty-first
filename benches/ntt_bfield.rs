8258ef352464fa0d39c99152cc422545f9744a7f
Simon Shine <simon@neptune.cash>
2022-02-02 23:11:34 +0100

    ntt: Fix ntt_forward benchmark

--- benches/ntt_bfield.rs
+++ benches/ntt_bfield.rs
@@ -1,94 +1,73 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use twenty_first::shared_math::{
-    b_field_element::BFieldElement,
-    ntt,
-    traits::{GetPrimitiveRootOfUnity, GetRandomElements},
-    x_field_element::XFieldElement,
+use criterion::measurement::WallTime;
+use criterion::{
+    criterion_group, criterion_main, BenchmarkGroup, BenchmarkId, Criterion, Throughput,
 };
+use twenty_first::shared_math::b_field_element::BFieldElement;
+use twenty_first::shared_math::ntt::ntt;
+use twenty_first::shared_math::traits::{GetPrimitiveRootOfUnity, GetRandomElements};
+use twenty_first::shared_math::x_field_element::XFieldElement;
 
-fn generate_ntt_input<'a>(log2_of_size: usize) -> Vec<BFieldElement> {
-    let size: usize = 2usize.pow(log2_of_size as u32);
-    let mut output: Vec<BFieldElement> = Vec::with_capacity(size);
-    for _ in 0..size {
-        let rand = rand::random::<u64>() as u128 % BFieldElement::QUOTIENT;
-        output.push(BFieldElement::new(rand));
+fn chu_ntt_forward(c: &mut Criterion) {
+    let mut group = c.benchmark_group("chu_ntt_forward");
+
+    let log2_of_sizes: Vec<usize> = vec![3, 5, 7, 12, 13, 16, 18];
+
+    // Benchmarking forward ntt on BFieldElements
+    for &log2_of_size in log2_of_sizes.iter() {
+        bfield_benchmark(
+            &mut group,
+            BenchmarkId::new("bfield", log2_of_size),
+            log2_of_size,
+        );
     }
 
-    output
-}
-
-fn generate_xfield_elements<'a>(log2_of_size: usize) -> Vec<XFieldElement> {
-    let size: usize = 2usize.pow(log2_of_size as u32);
-    let mut rng = rand::thread_rng();
-    XFieldElement::random_elements(size, &mut rng)
-}
-
-fn ntt_forward(c: &mut Criterion) {
-    let mut group = c.benchmark_group("ntt_bfield");
-
-    for &log2_of_size in [3, 5, 7, 12usize, 13, 16, 18].iter() {
-        let size = 2u128.pow(log2_of_size as u32);
-        let (unity_root, _) = XFieldElement::ring_zero().get_primitive_root_of_unity(size);
-        let mut input = generate_xfield_elements(log2_of_size);
-        group.throughput(Throughput::Elements(size as u64));
-        group
-            .bench_with_input(
-                BenchmarkId::new("chu_ntt x-field", log2_of_size),
-                &size,
-                |b, _| {
-                    b.iter(|| {
-                        ntt::ntt::<XFieldElement>(
-                            &mut input,
-                            unity_root.unwrap(),
-                            log2_of_size as u32,
-                        )
-                    });
-                },
-            )
-            .sample_size(10);
+    // Benchmarking forward ntt on XFieldElements
+    for &log2_of_size in log2_of_sizes.iter() {
+        xfield_benchmark(
+            &mut group,
+            BenchmarkId::new("xfield", log2_of_size),
+            log2_of_size,
+        );
     }
 
-    for &log2_of_size in [3, 5, 7, 12usize, 13, 16, 18].iter() {
-        let size = 2u128.pow(log2_of_size as u32);
-        let (unity_root, _) = BFieldElement::ring_zero().get_primitive_root_of_unity(size);
-        let mut input = generate_ntt_input(log2_of_size);
-        group.throughput(Throughput::Elements(size as u64));
-        group
-            .bench_with_input(
-                BenchmarkId::new("chu_ntt b-field", log2_of_size),
-                &size,
-                |b, _| {
-                    b.iter(|| {
-                        ntt::ntt::<BFieldElement>(
-                            &mut input,
-                            unity_root.unwrap(),
-                            log2_of_size as u32,
-                        )
-                    });
-                },
-            )
-            .sample_size(10);
-    }
-
-    for &log2_of_size in [3, 5, 7, 12usize, 13, 16, 18].iter() {
-        let size = 2u128.pow(log2_of_size as u32);
-        let (unity_root, _) = BFieldElement::ring_zero().get_primitive_root_of_unity(size);
-        let input = generate_ntt_input(log2_of_size);
-        group.throughput(Throughput::Elements(size as u64));
-        group
-            .bench_with_input(
-                BenchmarkId::new("ntt_bfield", log2_of_size),
-                &size,
-                |b, _| {
-                    b.iter(|| {
-                        twenty_first::shared_math::ntt::slow_ntt(&input, &unity_root.unwrap())
-                    });
-                },
-            )
-            .sample_size(10);
-    }
     group.finish();
 }
 
-criterion_group!(benches, ntt_forward);
+fn bfield_benchmark(
+    group: &mut BenchmarkGroup<WallTime>,
+    bench_id: BenchmarkId,
+    log2_of_size: usize,
+) {
+    let size: usize = 1 << log2_of_size;
+
+    let mut rng = rand::thread_rng();
+    let mut xs = BFieldElement::random_elements(size, &mut rng);
+    let omega = xs[0].get_primitive_root_of_unity(size as u128).0.unwrap();
+
+    group.throughput(Throughput::Elements(size as u64));
+    group.bench_with_input(bench_id, &size, |b, _| {
+        b.iter(|| ntt::<BFieldElement>(&mut xs, omega, log2_of_size as u32))
+    });
+    group.sample_size(10);
+}
+
+fn xfield_benchmark(
+    group: &mut BenchmarkGroup<WallTime>,
+    bench_id: BenchmarkId,
+    log2_of_size: usize,
+) {
+    let size: usize = 1 << log2_of_size;
+
+    let mut rng = rand::thread_rng();
+    let mut xs = XFieldElement::random_elements(size, &mut rng);
+    let omega = xs[0].get_primitive_root_of_unity(size as u128).0.unwrap();
+
+    group.throughput(Throughput::Elements(size as u64));
+    group.bench_with_input(bench_id, &size, |b, _| {
+        b.iter(|| ntt::<XFieldElement>(&mut xs, omega, log2_of_size as u32))
+    });
+    group.sample_size(10);
+}
+
+criterion_group!(benches, chu_ntt_forward);
 criterion_main!(benches);
