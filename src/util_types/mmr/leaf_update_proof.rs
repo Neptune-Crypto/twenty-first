50f3797d5790bf7fa5fab1d7b8e47c95454fad34
sword-smith <thor@neptune.cash>
2022-03-08 12:16:54 +0100

    Add equality trait to leaf update proof
    This will mainly be used in tests as there are multiple ways to generate
    the leaf update proofs, and we want an easy way to check if they're the
    same.

--- src/util_types/mmr/leaf_update_proof.rs
+++ src/util_types/mmr/leaf_update_proof.rs
@@ -21,6 +21,20 @@ where
     pub new_peaks: Vec<HashDigest>,
 }
 
+impl<HashDigest: PartialEq, H> PartialEq for LeafUpdateProof<HashDigest, H>
+where
+    HashDigest: ToDigest<HashDigest> + PartialEq + Clone + Debug,
+    H: Hasher<Digest = HashDigest> + Clone,
+{
+    // Two leaf update proofs are considered equal if they contain the same membership
+    // proofs, same new peaks, and same old peaks
+    fn eq(&self, other: &Self) -> bool {
+        self.membership_proof == other.membership_proof
+            && self.old_peaks == other.old_peaks
+            && self.new_peaks == other.new_peaks
+    }
+}
+
 impl<HashDigest, H> LeafUpdateProof<HashDigest, H>
 where
     HashDigest: ToDigest<HashDigest> + PartialEq + Clone + Debug,
@@ -57,5 +71,83 @@ where
         calculated_new_peaks[modified_peak_index] = sub_tree_root;
 
         calculated_new_peaks == self.new_peaks
+    }
+}
+
+#[cfg(test)]
+mod mmr_leaf_update_tests {
+    use crate::util_types::mmr::archive_mmr::MmrArchive;
+
+    use super::*;
+
+    #[test]
+    fn equality_test() {
+        let leaf_hashes: Vec<blake3::Hash> = vec![14u128, 15u128, 16u128]
+            .iter()
+            .map(|x| blake3::hash(bincode::serialize(x).expect("Encoding failed").as_slice()))
+            .collect();
+        let new_leaf = blake3::hash(
+            bincode::serialize(&1000u128)
+                .expect("Encoding failed")
+                .as_slice(),
+        );
+        let mut archival_mmr = MmrArchive::<blake3::Hash, blake3::Hasher>::new(leaf_hashes.clone());
+        let (mp0_old, peaks0_old) = archival_mmr.prove_membership(0);
+        archival_mmr.append(new_leaf);
+        let (mp0_new, peaks0_new) = archival_mmr.prove_membership(0);
+
+        // Test equality
+        // Note that these proofs do not have to be valid proofs
+        let ulp0 = LeafUpdateProof {
+            membership_proof: mp0_old.clone(),
+            new_peaks: peaks0_old.clone(),
+            old_peaks: peaks0_old.clone(),
+        };
+        let ulp1 = LeafUpdateProof {
+            membership_proof: mp0_old.clone(),
+            new_peaks: peaks0_new.clone(),
+            old_peaks: peaks0_old.clone(),
+        };
+        let ulp2 = LeafUpdateProof {
+            membership_proof: mp0_old.clone(),
+            new_peaks: peaks0_old.clone(),
+            old_peaks: peaks0_new.clone(),
+        };
+        let ulp3 = LeafUpdateProof {
+            membership_proof: mp0_new.clone(),
+            new_peaks: peaks0_old.clone(),
+            old_peaks: peaks0_old.clone(),
+        };
+        let ulp4 = LeafUpdateProof {
+            membership_proof: mp0_old.clone(),
+            new_peaks: peaks0_old.clone(),
+            old_peaks: peaks0_old.clone(),
+        };
+
+        assert_ne!(
+            ulp0, ulp1,
+            "leaf update proofs must be different when new peaks differ"
+        );
+        assert_ne!(
+            ulp1, ulp2,
+            "leaf update proofs must be different when old and new peaks differ"
+        );
+        assert_ne!(
+            ulp2, ulp3,
+            "leaf update proofs must be different when membership proof and old peaks differ"
+        );
+        assert_ne!(
+            ulp3, ulp4,
+            "leaf update proofs must be different when membership proof differ"
+        );
+        assert_eq!(
+            ulp4, ulp0,
+            "leaf update proofs must be equal when all three field agree"
+        );
+
+        assert_ne!(
+            ulp0, ulp2,
+            "leaf update proofs must be different when old peaks differ"
+        );
     }
 }
