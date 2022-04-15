6ec33b9eedab364d0c05b119080290e7cb5b2bb6
sword-smith <thor@neptune.cash>
2022-04-15 20:24:24 +0200

    Start profiling work

--- benches/stark_bf.rs
+++ benches/stark_bf.rs
@@ -3,20 +3,37 @@ use twenty_first::shared_math::b_field_element::BFieldElement;
 use twenty_first::shared_math::stark::brainfuck;
 use twenty_first::shared_math::stark::brainfuck::stark::Stark;
 use twenty_first::shared_math::stark::brainfuck::vm::BaseMatrices;
+use twenty_first::timing_reporter::TimingReporter;
 
 fn compile_simulate_prove_verify(program_code: &str, input: &[BFieldElement]) {
+    let mut timer = TimingReporter::start();
+
     let program = brainfuck::vm::compile(program_code).unwrap();
+
+    timer.elapsed("compile");
+
     let (trace_length, input_symbols, output_symbols) =
         brainfuck::vm::run(&program, input.to_vec()).unwrap();
+    timer.elapsed("run");
+
     let base_matrices: BaseMatrices = brainfuck::vm::simulate(&program, &input_symbols).unwrap();
+    timer.elapsed("simulate");
+
     let mut stark = Stark::new(
         trace_length,
         program_code.to_string(),
         input_symbols,
         output_symbols,
     );
+    timer.elapsed("new");
+
     let mut proof_stream = stark.prove(base_matrices).unwrap();
+    timer.elapsed("prove");
+
     let verifier_verdict = stark.verify(&mut proof_stream);
+    timer.elapsed("verify");
+    timer.finish();
+
     match verifier_verdict {
         Ok(_) => (),
         Err(err) => panic!("error in STARK verifier: {}", err),
