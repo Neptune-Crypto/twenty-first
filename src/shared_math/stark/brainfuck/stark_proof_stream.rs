efc30242243685fac54f508693e6103ff5ca723a
sword-smith <thor@neptune.cash>
2022-04-25 23:38:53 +0200

    Add three type constructors to STARK proof stream
    These will be used to compress some of the authentication paths in the
    BF STARK proof.

--- src/shared_math/stark/brainfuck/stark_proof_stream.rs
+++ src/shared_math/stark/brainfuck/stark_proof_stream.rs
@@ -9,9 +9,13 @@ use itertools::Itertools;
 pub type StarkProofStream = ProofStream<Item, RescuePrimeXlix<RP_DEFAULT_WIDTH>>;
 
 type FriProof = Vec<(PartialAuthenticationPath<Vec<BFieldElement>>, XFieldElement)>;
+type CompressedAuthenticationPaths = Vec<PartialAuthenticationPath<Vec<BFieldElement>>>;
 
 #[derive(Debug, Clone)]
 pub enum Item {
+    CompressedAuthenticationPaths(CompressedAuthenticationPaths),
+    TransposedBaseElementVectors(Vec<Vec<BFieldElement>>),
+    TransposedExtensionElementVectors(Vec<Vec<XFieldElement>>),
     MerkleRoot(Vec<BFieldElement>),
     Terminals([XFieldElement; TERMINAL_COUNT]),
     TransposedBaseElements(Vec<BFieldElement>),
@@ -23,6 +27,39 @@ pub enum Item {
 }
 
 impl Item {
+    pub fn as_compressed_authentication_paths(
+        &self,
+    ) -> Result<CompressedAuthenticationPaths, Box<dyn std::error::Error>> {
+        match self {
+            Self::CompressedAuthenticationPaths(caps) => Ok(caps.to_owned()),
+            _ => Err(ProofStreamError::boxed(
+                "expected compressed authentication paths, but got something else",
+            )),
+        }
+    }
+
+    pub fn as_transposed_base_element_vectors(
+        &self,
+    ) -> Result<Vec<Vec<BFieldElement>>, Box<dyn std::error::Error>> {
+        match self {
+            Self::TransposedBaseElementVectors(bss) => Ok(bss.to_owned()),
+            _ => Err(ProofStreamError::boxed(
+                "expected transposed base element vectors, but got something else",
+            )),
+        }
+    }
+
+    pub fn as_transposed_extension_element_vectors(
+        &self,
+    ) -> Result<Vec<Vec<XFieldElement>>, Box<dyn std::error::Error>> {
+        match self {
+            Self::TransposedExtensionElementVectors(xss) => Ok(xss.to_owned()),
+            _ => Err(ProofStreamError::boxed(
+                "expected transposed extension element vectors, but got something else",
+            )),
+        }
+    }
+
     pub fn as_merkle_root(&self) -> Result<Vec<BFieldElement>, Box<dyn std::error::Error>> {
         match self {
             Self::MerkleRoot(bs) => Ok(bs.clone()),
@@ -133,6 +170,24 @@ impl IntoIterator for Item {
 
                 bs.into_iter()
             }
+            Item::CompressedAuthenticationPaths(partial_auth_paths) => {
+                // TODO: An iterator can be derived without re-creating a &mut Vec<BFieldElement>.
+                let mut bs: Vec<BFieldElement> = vec![];
+
+                for partial_auth_path in partial_auth_paths.iter() {
+                    for bs_in_partial_auth_path in partial_auth_path.0.iter().flatten() {
+                        bs.append(&mut bs_in_partial_auth_path.clone());
+                    }
+                }
+
+                bs.into_iter()
+            }
+            Item::TransposedBaseElementVectors(bss) => bss.concat().into_iter(),
+            Item::TransposedExtensionElementVectors(xss) => xss
+                .into_iter()
+                .map(|xs| xs_to_bs(&xs).collect::<Vec<_>>())
+                .concat()
+                .into_iter(),
         }
     }
 }
