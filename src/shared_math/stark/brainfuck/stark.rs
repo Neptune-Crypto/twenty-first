abb5a2f3f7a56b8e82be6b55b853316fc3c81852
sword-smith <thor@neptune.cash>
2022-05-10 10:48:11 +0200

    Improve timing report identifier for non-linear combination calculation

--- src/shared_math/stark/brainfuck/stark.rs
+++ src/shared_math/stark/brainfuck/stark.rs
@@ -515,7 +515,7 @@ impl Stark {
             weights_counter += 1;
         }
 
-        timer.elapsed("...shift and interpolate base codewords?");
+        timer.elapsed("...shift and collect base codewords");
 
         assert_eq!(extension_codewords.len(), num_extension_polynomials);
         for (i, (ec, edb)) in extension_codewords
@@ -556,7 +556,7 @@ impl Stark {
             weights_counter += 1;
         }
 
-        timer.elapsed("...shift and interpolate extension codewords?");
+        timer.elapsed("...shift and collect extension codewords");
 
         assert_eq!(quotient_codewords.len(), num_quotient_polynomials);
         for (_i, (qc, qdb)) in quotient_codewords
@@ -606,7 +606,7 @@ impl Stark {
             weights_counter += 1;
         }
 
-        timer.elapsed("...shift (~and interpolate~) quotient codewords?");
+        timer.elapsed("...shift and collect quotient codewords");
 
         // assert_eq!(
         //     terms.len(),
