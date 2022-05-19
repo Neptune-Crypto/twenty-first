3792566d2f468fd8f25f5d597fa9eeb957d0b093
Simon Shine <shreddedglory@gmail.com>
2022-05-19 17:32:41 +0200

    WIP: Test ProcessorTable

--- src/shared_math/stark/triton/table/processor_table.rs
+++ src/shared_math/stark/triton/table/processor_table.rs
@@ -91,3 +91,17 @@ impl Table<XFieldElement, FULL_WIDTH> for ExtProcessorTable {
 }
 
 impl ExtensionTable<FULL_WIDTH> for ExtProcessorTable {}
+
+#[cfg(test)]
+mod processor_table_tests {
+    use super::*;
+
+    #[test]
+    fn initialise_table_test() {
+        // 1. Execute program
+
+        // 2. Convert to table
+        // 3. Extract constraints
+        // 4. Check constraints
+    }
+}
