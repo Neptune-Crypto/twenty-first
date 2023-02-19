4ede7993664731cf31eb406bbe2bb6df177ad78e
Alan Szepieniec <alan@neptune.cash>
2023-02-19 19:25:08 +0100

    add module mathematics

--- twenty-first/src/shared_math/lattice.rs
+++ twenty-first/src/shared_math/lattice.rs
@@ -1,5 +1,8 @@
+use std::ops::{Add, AddAssign, Mul};
+
 use itertools::Itertools;
 use num_traits::Zero;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 use super::b_field_element::BFieldElement;
 
@@ -189,37 +192,104 @@ pub fn coset_ntt_noswap_64(array: &mut [BFieldElement; 64]) {
     }
 }
 
-pub fn had64(a: &[BFieldElement; 64], b: &[BFieldElement; 64]) -> [BFieldElement; 64] {
-    let mut c = [BFieldElement::zero(); 64];
-    for i in 0..64 {
-        c[i] = a[i] * b[i];
-    }
-    c
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct CyclotomicRingElement {
+    coefficients: [BFieldElement; 64],
 }
 
-pub fn add64(a: &[BFieldElement; 64], b: &[BFieldElement; 64]) -> [BFieldElement; 64] {
-    let mut c = [BFieldElement::zero(); 64];
-    for i in 0..64 {
-        c[i] = a[i] + b[i];
+impl CyclotomicRingElement {
+    pub fn sample_short(randomness: &[u8; 8 * 64]) -> CyclotomicRingElement {
+        CyclotomicRingElement {
+            coefficients: randomness
+                .chunks(8)
+                .into_iter()
+                .map(|r| TryInto::<[u8; 8]>::try_into(r).unwrap())
+                .map(|r| sample_short_bfield_element(&r))
+                .collect_vec()
+                .try_into()
+                .unwrap(),
+        }
     }
-    c
+
+    pub fn sample_uniform(randomness: &[u8]) -> CyclotomicRingElement {
+        debug_assert!(randomness.len() >= 9 * 64);
+        let mut coefficients = [BFieldElement::zero(); 64];
+        for i in 0..64 {
+            let mut acc = 0u128;
+            for j in 0..9 {
+                acc = acc * 256 + randomness[i * 9 + j] as u128;
+            }
+            acc %= BFieldElement::P as u128;
+            coefficients[i] = BFieldElement::new(acc as u64);
+        }
+        CyclotomicRingElement { coefficients }
+    }
+
+    pub fn hadamard(a: CyclotomicRingElement, b: CyclotomicRingElement) -> CyclotomicRingElement {
+        let mut c = CyclotomicRingElement::zero();
+        for i in 0..64 {
+            c.coefficients[i] = a.coefficients[i] * b.coefficients[i];
+        }
+        c
+    }
 }
 
-/// Multiply two polynomials in the ring
-/// Fp[X] / (X^64 + 1)
-/// using coset-NTT.
-pub fn cycloring64_mul(a: &[BFieldElement; 64], b: &[BFieldElement; 64]) -> [BFieldElement; 64] {
-    let mut a_copy = *a;
-    let mut b_copy = *b;
+impl Add for CyclotomicRingElement {
+    type Output = CyclotomicRingElement;
 
-    coset_ntt_noswap_64(&mut a_copy);
-    coset_ntt_noswap_64(&mut b_copy);
+    fn add(self, rhs: Self) -> Self::Output {
+        CyclotomicRingElement {
+            coefficients: (0..64)
+                .into_iter()
+                .map(|i| self.coefficients[i] + rhs.coefficients[i])
+                .collect_vec()
+                .try_into()
+                .unwrap(),
+        }
+    }
+}
 
-    let mut c = had64(&a_copy, &b_copy);
+impl AddAssign for CyclotomicRingElement {
+    fn add_assign(&mut self, rhs: Self) {
+        self.coefficients
+            .iter_mut()
+            .zip(rhs.coefficients.iter())
+            .for_each(|(l, r)| *l += *r);
+    }
+}
 
-    coset_intt_noswap_64(&mut c);
+impl Mul for CyclotomicRingElement {
+    type Output = CyclotomicRingElement;
 
-    c
+    /// Multiply two polynomials in the ring
+    /// Fp[X] / (X^64 + 1)
+    /// using coset-NTT.
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut lhs_coeffs = self.coefficients;
+        let mut rhs_coeffs = rhs.coefficients;
+        coset_ntt_noswap_64(&mut lhs_coeffs);
+        coset_ntt_noswap_64(&mut rhs_coeffs);
+        let mut out_coeffs = [BFieldElement::zero(); 64];
+        for i in 0..64 {
+            out_coeffs[i] = lhs_coeffs[i] * rhs_coeffs[i];
+        }
+        coset_intt_noswap_64(&mut out_coeffs);
+        CyclotomicRingElement {
+            coefficients: out_coeffs,
+        }
+    }
+}
+
+impl Zero for CyclotomicRingElement {
+    fn zero() -> Self {
+        CyclotomicRingElement {
+            coefficients: [BFieldElement::zero(); 64],
+        }
+    }
+
+    fn is_zero(&self) -> bool {
+        self.coefficients == [BFieldElement::zero(); 64]
+    }
 }
 
 pub fn embed_msg(msg: [u8; 32]) -> [BFieldElement; 64] {
@@ -297,7 +367,7 @@ const fn num_set_bits_table() -> [u8; 256] {
     table
 }
 
-fn sample_short_bfield_element(randomness: &[u8; 8]) -> BFieldElement {
+pub fn sample_short_bfield_element(randomness: &[u8; 8]) -> BFieldElement {
     const NUM_SET_BITS: [u8; 256] = num_set_bits_table();
     let left = ((NUM_SET_BITS[randomness[0] as usize] as u64) << (3 * 16))
         + ((NUM_SET_BITS[randomness[1] as usize] as u64) << (2 * 16))
@@ -310,15 +380,185 @@ fn sample_short_bfield_element(randomness: &[u8; 8]) -> BFieldElement {
     BFieldElement::new(left) - BFieldElement::new(right)
 }
 
-fn sample_short_cycloring_element(randomness: &[u8; 8 * 64]) -> [BFieldElement; 64] {
-    randomness
-        .chunks(8)
-        .into_iter()
-        .map(|r| TryInto::<[u8; 8]>::try_into(r).unwrap())
-        .map(|r| sample_short_bfield_element(&r))
-        .collect_vec()
-        .try_into()
-        .unwrap()
+/// The Module is a matrix over the cyclotomic ring (i.e., the ring
+/// of residue classes of polynomials modulo X^64+1). The matrix
+/// contains N cyclotomic ring elements in total.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct ModuleElement<const N: usize> {
+    elements: [CyclotomicRingElement; N],
+}
+
+impl<const N: usize> ModuleElement<N> {
+    pub fn sample_short(randomness: &[[u8; 8 * 64]; N]) -> Self {
+        let mut elements = [CyclotomicRingElement::zero(); N];
+        for n in 0..N {
+            elements[n] = CyclotomicRingElement::sample_short(&randomness[n]);
+        }
+        Self { elements }
+    }
+
+    pub fn sample_uniform(randomness: &[u8]) -> Self {
+        debug_assert!(randomness.len() >= N * 9 * 64);
+        ModuleElement {
+            elements: (0..N)
+                .into_iter()
+                .map(|i| {
+                    CyclotomicRingElement::sample_uniform(&randomness[i * 9 * 64..(i + 1) * 9 * 64])
+                })
+                .collect_vec()
+                .try_into()
+                .unwrap(),
+        }
+    }
+
+    pub fn ntt(&self) -> Self {
+        let mut copy = *self;
+        for n in 0..N {
+            coset_ntt_noswap_64(&mut copy.elements[n].coefficients);
+        }
+        copy
+    }
+
+    pub fn intt(&self) -> Self {
+        let mut copy = *self;
+        for n in 0..N {
+            coset_intt_noswap_64(&mut copy.elements[n].coefficients);
+        }
+        copy
+    }
+
+    /// Multiply two module elements from a pair of matrix-
+    /// multiplication-compatible modules. This method uses
+    /// hadamard multiplication for cyclotomic ring elements, which
+    /// is useful for avoiding the repeated conversion to and from
+    /// NTT domain.
+    ///  - `N` counts the total number of elements in the matrix;
+    ///  - `H` counts the number of rows of the left hand side (and of
+    ///  the output) matrix;
+    ///  - `W` counts the number of columns of the right hand side (and
+    ///  of the output) matrix;
+    ///  - `INNER` counts the number of columns of the left hand side,
+    ///  as well as the number of rows of the right hand side.
+    pub fn multiply_hadamard<
+        const LHS_H: usize,
+        const LHS_N: usize,
+        const RHS_W: usize,
+        const RHS_N: usize,
+        const INNER: usize,
+        const OUT_N: usize,
+    >(
+        lhs: ModuleElement<LHS_N>,
+        rhs: ModuleElement<RHS_N>,
+    ) -> ModuleElement<OUT_N> {
+        debug_assert_eq!(LHS_H * INNER, LHS_N);
+        debug_assert_eq!(INNER * RHS_W, RHS_N);
+        debug_assert_eq!(LHS_H * RHS_W, OUT_N);
+
+        let mut elements = [CyclotomicRingElement::zero(); OUT_N];
+        for h in 0..LHS_H {
+            for w in 0..RHS_W {
+                for i in 0..INNER {
+                    elements[h * RHS_W + w] += CyclotomicRingElement::hadamard(
+                        lhs.elements[h * INNER + i],
+                        rhs.elements[i * RHS_W + w],
+                    );
+                }
+            }
+        }
+
+        ModuleElement { elements }
+    }
+
+    /// Multiply two module elements from a pair of matrix-
+    /// multiplication-compatible modules. This method uses the
+    /// multiplication defined for cyclotomic ring elements
+    /// abstractly. For a faster method that computes the entire
+    /// matrix multiplication in the NTT domain, use `fast_multiply`.
+    ///  - `N` counts the total number of elements in the matrix;
+    ///  - `H` counts the number of rows of the left hand side (and of
+    ///  the output) matrix;
+    ///  - `W` counts the number of columns of the right hand side (and
+    ///  of the output) matrix;
+    ///  - `INNER` counts the number of columns of the left hand side,
+    ///  as well as the number of rows of the right hand side.
+    pub fn multiply<
+        const LHS_H: usize,
+        const LHS_N: usize,
+        const RHS_W: usize,
+        const RHS_N: usize,
+        const INNER: usize,
+        const OUT_N: usize,
+    >(
+        lhs: ModuleElement<LHS_N>,
+        rhs: ModuleElement<RHS_N>,
+    ) -> ModuleElement<OUT_N> {
+        debug_assert_eq!(LHS_H * INNER, LHS_N);
+        debug_assert_eq!(INNER * RHS_W, RHS_N);
+        debug_assert_eq!(LHS_H * RHS_W, OUT_N);
+
+        let mut out = ModuleElement {
+            elements: [CyclotomicRingElement::zero(); OUT_N],
+        };
+        for h in 0..LHS_H {
+            for w in 0..RHS_W {
+                for i in 0..INNER {
+                    out.elements[h * RHS_W + w] +=
+                        lhs.elements[h * INNER + i] * rhs.elements[i * RHS_W + w];
+                }
+            }
+        }
+
+        out
+    }
+
+    /// Multiply two module elements from a pair of matrix-
+    /// multiplication-compatible modules, by converting everything
+    /// into the NTT domain, performing the matrix multiplication,
+    /// and converting back.
+    ///  - `N` counts the total number of elements in the matrix;
+    ///  - `H` counts the number of rows of the left hand side (and of
+    ///  the output) matrix;
+    ///  - `W` counts the number of columns of the right hand side (and
+    ///  of the output) matrix;
+    ///  - `INNER` counts the number of columns of the left hand side,
+    ///  as well as the number of rows of the right hand side.
+    pub fn fast_multiply<
+        const LHS_H: usize,
+        const LHS_N: usize,
+        const RHS_W: usize,
+        const RHS_N: usize,
+        const INNER: usize,
+        const OUT_N: usize,
+    >(
+        lhs: ModuleElement<LHS_N>,
+        rhs: ModuleElement<RHS_N>,
+    ) -> ModuleElement<OUT_N> {
+        debug_assert_eq!(LHS_H * INNER, LHS_N);
+        debug_assert_eq!(INNER * RHS_W, RHS_N);
+        debug_assert_eq!(LHS_H * RHS_W, OUT_N);
+
+        let lhs_ntt = lhs.ntt();
+        let rhs_ntt = rhs.ntt();
+
+        let out_ntt =
+            Self::multiply_hadamard::<LHS_H, LHS_N, RHS_W, RHS_N, INNER, OUT_N>(lhs_ntt, rhs_ntt);
+
+        out_ntt.intt()
+    }
+}
+
+impl<const N: usize> Add for ModuleElement<N> {
+    type Output = ModuleElement<N>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let elements: [CyclotomicRingElement; N] = (0..N)
+            .into_par_iter()
+            .map(|i| self.elements[i] + rhs.elements[i])
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+        ModuleElement::<N> { elements }
+    }
 }
 
 #[cfg(test)]
@@ -347,7 +587,9 @@ mod lattice_test {
             }
         }
 
-        let c_fast = cycloring64_mul(&a, &b);
+        let c_fast = (CyclotomicRingElement { coefficients: a }
+            * CyclotomicRingElement { coefficients: b })
+        .coefficients;
 
         assert_eq!(c_fast, c_schoolbook);
     }
@@ -365,5 +607,49 @@ mod lattice_test {
         let extracted = extract_msg(embedding);
 
         assert_eq!(msg, extracted);
+    }
+
+    #[test]
+    fn test_module_distributivity() {
+        let mut rng = thread_rng();
+        let randomness = (0..(2 * 3 + 2 * 3 + 3 * 1) * 64 * 9)
+            .into_iter()
+            .map(|_| (rng.next_u32() % 256) as u8)
+            .collect_vec();
+        let mut start = 0;
+        let mut stop = 2 * 3 * 9 * 64;
+        let a = ModuleElement::<{ 2 * 3 }>::sample_uniform(&randomness[start..stop]);
+        start = stop;
+        stop += 2 * 3 * 9 * 64;
+        let b = ModuleElement::<{ 2 * 3 }>::sample_uniform(&randomness[start..stop]);
+        start = stop;
+        stop += 3 * 1 * 9 * 64;
+        let c = ModuleElement::<{ 3 * 1 }>::sample_uniform(&randomness[start..stop]);
+
+        let sumprod = ModuleElement::<1>::multiply::<2, 6, 1, 3, 3, 2>(a + b, c);
+        let prodsum = ModuleElement::<1>::multiply::<2, 6, 1, 3, 3, 2>(a, c)
+            + ModuleElement::<1>::multiply::<2, 6, 1, 3, 3, 2>(b, c);
+
+        assert_eq!(sumprod, prodsum);
+    }
+
+    #[test]
+    fn test_module_multiply() {
+        let mut rng = thread_rng();
+        let randomness = (0..(2 * 3 + 2 * 3 + 3 * 1) * 64 * 9)
+            .into_iter()
+            .map(|_| (rng.next_u32() % 256) as u8)
+            .collect_vec();
+        let mut start = 0;
+        let mut stop = 2 * 3 * 9 * 64;
+        let a = ModuleElement::<{ 2 * 3 }>::sample_uniform(&randomness[start..stop]);
+        start = stop;
+        stop += 3 * 2 * 9 * 64;
+        let b = ModuleElement::<{ 2 * 3 }>::sample_uniform(&randomness[start..stop]);
+
+        assert_eq!(
+            ModuleElement::<1>::fast_multiply::<2, 6, 2, 6, 3, 4>(a, b),
+            ModuleElement::<1>::multiply::<2, 6, 2, 6, 3, 4>(a, b)
+        );
     }
 }
