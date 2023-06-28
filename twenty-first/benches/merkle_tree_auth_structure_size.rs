e5f5b38dd2b2347b2e54c8759ca99730dd896f86
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-06-28 16:15:52 +0200

    add benchmark for the encoded size of a Merkle tree's auth structure

---
+++ twenty-first/benches/merkle_tree_auth_structure_size.rs
@@ -0,0 +1,117 @@
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::measurement::Measurement;
+use criterion::measurement::ValueFormatter;
+use criterion::BenchmarkId;
+use criterion::Criterion;
+use criterion::Throughput;
+use itertools::Itertools;
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::RngCore;
+use rand::SeedableRng;
+use twenty_first::shared_math::bfield_codec::BFieldCodec;
+
+use twenty_first::shared_math::tip5::Tip5;
+use twenty_first::util_types::algebraic_hasher::AlgebraicHasher;
+use twenty_first::util_types::merkle_tree::CpuParallel;
+use twenty_first::util_types::merkle_tree::MerkleTree;
+use twenty_first::util_types::merkle_tree_maker::MerkleTreeMaker;
+
+#[derive(Debug, Clone, Copy)]
+struct AuthStructureEncodingLength(f64);
+
+#[derive(Debug, Clone, Copy)]
+struct AuthStructureEncodingLengthFormatter;
+
+impl Measurement for AuthStructureEncodingLength {
+    type Intermediate = ();
+    type Value = Self;
+
+    fn start(&self) -> Self::Intermediate {
+        ()
+    }
+
+    fn end(&self, _i: Self::Intermediate) -> Self::Value {
+        self.to_owned()
+    }
+
+    fn add(&self, v1: &Self::Value, v2: &Self::Value) -> Self::Value {
+        AuthStructureEncodingLength(v1.0 + v2.0)
+    }
+
+    fn zero(&self) -> Self::Value {
+        AuthStructureEncodingLength(0.0)
+    }
+
+    fn to_f64(&self, value: &Self::Value) -> f64 {
+        value.0
+    }
+
+    fn formatter(&self) -> &dyn ValueFormatter {
+        &AuthStructureEncodingLengthFormatter
+    }
+}
+
+impl ValueFormatter for AuthStructureEncodingLengthFormatter {
+    fn scale_values(&self, _typical_value: f64, _values: &mut [f64]) -> &'static str {
+        "bfe"
+    }
+
+    fn scale_throughputs(
+        &self,
+        _typical_value: f64,
+        _throughput: &Throughput,
+        _values: &mut [f64],
+    ) -> &'static str {
+        "bfe/s"
+    }
+
+    fn scale_for_machines(&self, _values: &mut [f64]) -> &'static str {
+        "bfe"
+    }
+}
+
+fn auth_structure_len(c: &mut Criterion<AuthStructureEncodingLength>) {
+    let mut rng = StdRng::seed_from_u64(0);
+
+    let tree_height = 22;
+    let num_leaves = 1 << tree_height;
+    let leaves = (0..num_leaves).map(|_| rng.next_u64()).collect_vec();
+    let leaf_digests = leaves.iter().map(Tip5::hash).collect_vec();
+    let mt: MerkleTree<Tip5> = CpuParallel::from_digests(&leaf_digests);
+
+    let num_opened_indices = 40;
+    let mut group = c.benchmark_group("merkle_tree_auth_structure_size");
+    group.bench_function(
+        BenchmarkId::new("auth_structure_size", num_leaves),
+        |bencher| {
+            bencher.iter_custom(|iters| {
+                let mut total_len = AuthStructureEncodingLength(0.0);
+                for _ in 0..iters {
+                    let opened_indices = (0..num_opened_indices)
+                        .map(|_| rng.gen_range(0..num_leaves))
+                        .collect_vec();
+                    let auth_structure = mt.get_authentication_structure(&opened_indices);
+                    let this_len = auth_structure.encode().len();
+                    let this_len = AuthStructureEncodingLength(this_len as f64);
+                    total_len = total_len.add(&total_len, &this_len);
+                }
+                total_len
+            })
+        },
+    );
+}
+
+fn auth_structure_len_measurements() -> Criterion<AuthStructureEncodingLength> {
+    Criterion::default()
+        .with_measurement(AuthStructureEncodingLength(0.0))
+        .sample_size(100)
+}
+
+criterion_group!(
+    name = benches;
+    config =  auth_structure_len_measurements();
+    targets = auth_structure_len
+);
+criterion_main!(benches);
