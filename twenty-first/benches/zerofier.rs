e1ffd2d9e67eaba32b5b6df9e8e7180a4ece9064
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-11-13 15:04:58 +0100

    set up benchmark for zerofier computation
    benchmark
    
    zerofier 0      time:   [11.356 ns 11.362 ns 11.370 ns]
    
    zerofier 10     time:   [310.49 ns 310.60 ns 310.73 ns]
    
    zerofier 100    time:   [28.012 µs 28.024 µs 28.038 µs]
    
    zerofier 1000   time:   [2.7692 ms 2.7702 ms 2.7713 ms]
    
    zerofier 10000  time:   [273.90 ms 274.02 ms 274.14 ms]

---
+++ twenty-first/benches/zerofier.rs
@@ -0,0 +1,30 @@
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::BatchSize;
+use criterion::Criterion;
+use rand::thread_rng;
+use rand::Rng;
+use twenty_first::shared_math::b_field_element::BFieldElement;
+use twenty_first::shared_math::polynomial::Polynomial;
+
+criterion_main!(benches);
+criterion_group!(
+    benches,
+    zerofier<0>,
+    zerofier<10>,
+    zerofier<100>,
+    zerofier<1_000>,
+    zerofier<10_000>,
+);
+
+fn zerofier<const N: usize>(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let id = format!("zerofier {N}");
+    c.bench_function(&id, |b| {
+        b.iter_batched(
+            || rng.gen(),
+            |v: [BFieldElement; N]| Polynomial::zerofier(&v),
+            BatchSize::SmallInput,
+        )
+    });
+}
