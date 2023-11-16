c50f31f3ce063bbb58ae4570dcad4491a9f6bd6c
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-11-16 12:23:08 +0100

    set up benchmark for fast zerofier computation
    benchmark
    
    fast_zerofier 0       time:   [13.989 ns 14.010 ns 14.035 ns]
    
    fast_zerofier 10      time:   [1.4670 µs 1.4678 µs 1.4688 µs]
    
    fast_zerofier 100     time:   [31.246 µs 31.258 µs 31.272 µs]
    
    fast_zerofier 1000    time:   [553.03 µs 553.22 µs 553.43 µs]
    
    fast_zerofier 10000   time:   [10.975 ms 10.978 ms 10.982 ms]

--- twenty-first/benches/zerofier.rs
+++ twenty-first/benches/zerofier.rs
@@ -6,6 +6,7 @@ use rand::thread_rng;
 use rand::Rng;
 use twenty_first::shared_math::b_field_element::BFieldElement;
 use twenty_first::shared_math::polynomial::Polynomial;
+use twenty_first::shared_math::traits::PrimitiveRootOfUnity;
 
 criterion_main!(benches);
 criterion_group!(
@@ -15,6 +16,11 @@ criterion_group!(
     zerofier<100>,
     zerofier<1_000>,
     zerofier<10_000>,
+    fast_zerofier<0>,
+    fast_zerofier<10>,
+    fast_zerofier<100>,
+    fast_zerofier<1_000>,
+    fast_zerofier<10_000>,
 );
 
 fn zerofier<const N: usize>(c: &mut Criterion) {
@@ -24,6 +30,21 @@ fn zerofier<const N: usize>(c: &mut Criterion) {
         b.iter_batched(
             || rng.gen(),
             |v: [BFieldElement; N]| Polynomial::zerofier(&v),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn fast_zerofier<const N: usize>(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let root_order = (N + 1).next_power_of_two();
+    let root_of_unity = BFieldElement::primitive_root_of_unity(root_order as u64).unwrap();
+
+    let id = format!("fast_zerofier {N}");
+    c.bench_function(&id, |b| {
+        b.iter_batched(
+            || rng.gen(),
+            |v: [BFieldElement; N]| Polynomial::fast_zerofier(&v, root_of_unity, root_order),
             BatchSize::SmallInput,
         )
     });
