742e11eb9fb5bef8fb1f03e9b5b93069b1b5fa28
danda <dan-da@users.noreply.github.com>
2023-12-11 07:41:36 -0800

    feat: use bincode in RustyValue for all types
    This removes the manual serialization routines that maintained
    backwards compatibility with neptune-core alpha databases.

--- twenty-first/src/storage/storage_schema/rusty_value.rs
+++ twenty-first/src/storage/storage_schema/rusty_value.rs
@@ -2,7 +2,6 @@
 // in the database.  See type description below.
 //
 // RustyValue now makes use of `bincode` for serializing data.
-// There is a (temporary) caveat: see bottom of file.
 // Any rust `serializer` crate that understands serde
 // Serialize and Deserialize traits could be plugged in instead.
 //
@@ -35,11 +34,8 @@
 // todo: consider moving the serialization functions, or perhaps all
 // of rusty_value.rs to a top level module, eg twenty-first::serialization.
 
-use crate::shared_math::b_field_element::BFieldElement;
 use crate::shared_math::tip5;
-use itertools::Itertools;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::any::{Any, TypeId};
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
 use std::fmt::Debug;
 
@@ -135,6 +131,27 @@ pub fn deserialize<T: DeserializeOwned>(bytes: &[u8]) -> T {
     // ron::from_str(String::from_utf8(bytes.to_vec()).unwrap().as_str()).unwrap()
     // postcard::from_bytes(bytes).unwrap()
 }
+
+// todo: I *think* all of the following code can be removed.
+//
+// The original goal was to make RustyValue ::from() and ::into()
+// work for any type T: that impls Serialize and Deserialize.
+//
+// I was able to make it work for the ::from(), but not the ::into()
+// due to some rust rules.
+//
+// Details are here:
+// https://stackoverflow.com/questions/77624556/how-can-i-make-a-type-that-de-serializes-any-other-type-that-implements-serializ
+//
+// Now it seems there is an (obvious?) solution and I can make it work using inherent
+// methods, instead of `impl From`.
+//
+// Example:
+//  `fn from_any<T: Serialize>(value: &T) -> RustyValue` {..}
+//  `fn into_any<T: DeserializeOwned>(&self) -> T` {..}
+//
+// This should provide greater flexibility with less code.
+// I intend to prove it out today, and update PR if no problem.
 
 impl<K, T> From<BTreeMap<K, T>> for RustyValue
 where
@@ -262,8 +279,7 @@ macro_rules! impl_From_Generic {
     };
 }
 
-// impl_From_Generic!(Vec<T>, VecDeque<T>, LinkedList<T>);  // Vec<T> impl'd below, for now.
-impl_From_Generic!(VecDeque<T>, LinkedList<T>);
+impl_From_Generic!(Vec<T>, VecDeque<T>, LinkedList<T>);
 
 macro_rules! impl_From {
     ( $($type:ty),* $(,)? ) => {
@@ -287,140 +303,9 @@ macro_rules! impl_From {
     };
 }
 
-impl_From!(u8, u16, u32, u128, usize); // note: u64 is impl'd below (for now)
+impl_From!(u8, u16, u32, u64, u128, usize);
 impl_From!(i8, i16, i32, i64, i128, isize);
 impl_From!(bool);
 impl_From!(f32, f64);
 impl_From!(char, String);
-// impl_From!(tip5::Digest);  // note: impl'd below (for now)
-
-// ****** From impls without De/Serialize ******************
-//
-// These are the original RustyValue impls.
-//
-// All of these *can* be auto-impl'd above and code below disappears.
-// However that breaks compatibility with existing databases.
-//
-// In particular, without the code below, the neptune-core test
-// `can_initialize_mutator_set_database` would fail for a blockchain
-// synced before the serialization code above was in place.
-// That's because that test opens the user's "real" database and reads
-// in values, rather than generating and using a test DB, as other
-// tests do.
-//
-// For now we leave these in place until a decision is made if
-// we want to use a single serialization for all data
-// types.
-
-// Vec<T> is special because we want the auto serialization
-// for all types of T except Vec<u32>
-//
-// This is because neptune-core had a Vec<u32> impl for
-// RustyMSValue, but that type has gone away with the removal
-// of the ParentKey, ParentValue generics.  Since `From` and
-// `RustyValue` and `Vec` are all foreign to neptune-core,
-// it must be impl'd here, to get the same behavior.
-//
-// So we use some trickery from std::any::Any to detect and special
-// case this type.
-//
-// Todo: Vec<T> can/should be auto-impld above if we decide we do not
-// need to continue the original (manual) bytes serialization.
-//
-// Todo: If we want to keep the original (manual) bytes serialization,
-// it would be much better if we can use `downcast()` here instead of
-// `downcast_ref()` and avoid allocating with `to_vec()`.
-// When I tried using `downcast()`, I was initially unable to get
-// around some compiler errors, but I believe it should be possible.
-impl<T> From<Vec<T>> for RustyValue
-where
-    T: Serialize + 'static,
-{
-    #[inline]
-    fn from(value: Vec<T>) -> Self {
-        if let Some(value_u32) = (&value as &dyn Any).downcast_ref::<Vec<u32>>() {
-            Self(
-                value_u32
-                    .iter()
-                    .map(|&i| i.to_be_bytes())
-                    .collect_vec()
-                    .concat(),
-            )
-        } else {
-            RustyValue(serialize(&value))
-        }
-    }
-}
-impl<T> From<RustyValue> for Vec<T>
-where
-    T: DeserializeOwned + 'static + Clone,
-{
-    #[inline]
-    fn from(value: RustyValue) -> Self {
-        if TypeId::of::<Self>() == TypeId::of::<Vec<u32>>() {
-            let vec_u32 = value
-                .0
-                .chunks(4)
-                .map(|ch| {
-                    u32::from_be_bytes(
-                        ch.try_into()
-                            .expect("Cannot unpack RustyMSValue as Vec<u32>s"),
-                    )
-                })
-                .collect_vec();
-            (&vec_u32 as &dyn Any)
-                .downcast_ref::<Vec<T>>()
-                .unwrap()
-                .to_vec()
-        } else {
-            deserialize(&value.0)
-        }
-    }
-}
-
-impl From<RustyValue> for u64 {
-    #[inline]
-    fn from(value: RustyValue) -> Self {
-        u64::from_be_bytes(
-            value
-                .0
-                .try_into()
-                .expect("should have deserialized bytes into u64"),
-        )
-    }
-}
-impl From<u64> for RustyValue {
-    #[inline]
-    fn from(value: u64) -> Self {
-        RustyValue(value.to_be_bytes().to_vec())
-    }
-}
-
-impl From<RustyValue> for tip5::Digest {
-    #[inline]
-    fn from(value: RustyValue) -> Self {
-        tip5::Digest::new(
-            value
-                .0
-                .chunks(8)
-                .map(|ch| {
-                    u64::from_be_bytes(ch.try_into().expect("Cannot cast RustyValue into Digest"))
-                })
-                .map(BFieldElement::new)
-                .collect::<Vec<_>>()
-                .try_into().expect("Can cast RustyValue into BFieldElements but number does not match that of Digest."),
-        )
-    }
-}
-impl From<tip5::Digest> for RustyValue {
-    #[inline]
-    fn from(value: tip5::Digest) -> Self {
-        RustyValue(
-            value
-                .values()
-                .map(|b| b.value())
-                .map(u64::to_be_bytes)
-                .concat(),
-        )
-    }
-}
+impl_From!(tip5::Digest);
