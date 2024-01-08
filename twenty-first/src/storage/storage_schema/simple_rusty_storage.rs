e183018ce5ec6de1a4dfa4691c5aa9295e3766dc
danda <dan-da@users.noreply.github.com>
2024-01-08 13:47:27 -0700

    feat: make use of lock events in storage

--- twenty-first/src/storage/storage_schema/simple_rusty_storage.rs
+++ twenty-first/src/storage/storage_schema/simple_rusty_storage.rs
@@ -1,7 +1,7 @@
 use super::super::level_db::DB;
 use super::enums::WriteOperation;
 use super::{traits::StorageWriter, DbtSchema, SimpleRustyReader};
-use crate::sync::AtomicRw;
+use crate::sync::LockCallbackFn;
 use leveldb::batch::WriteBatch;
 use std::sync::Arc;
 
@@ -49,10 +49,19 @@ impl SimpleRustyStorage {
     /// Create a new SimpleRustyStorage
     #[inline]
     pub fn new(db: DB) -> Self {
-        let schema = DbtSchema::<SimpleRustyReader> {
-            tables: AtomicRw::from(Vec::new()),
-            reader: Arc::new(SimpleRustyReader { db }),
-        };
+        let schema =
+            DbtSchema::<SimpleRustyReader>::new(Arc::new(SimpleRustyReader { db }), None, None);
+        Self { schema }
+    }
+
+    /// Create a new SimpleRustyStorage and provide a
+    /// name and lock acquisition callback for tracing
+    pub fn new_with_callback(db: DB, storage_name: &str, lock_callback_fn: LockCallbackFn) -> Self {
+        let schema = DbtSchema::<SimpleRustyReader>::new(
+            Arc::new(SimpleRustyReader { db }),
+            Some(storage_name),
+            Some(lock_callback_fn),
+        );
         Self { schema }
     }
 
