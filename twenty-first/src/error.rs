0fef2d602745c11106497cc7894e99410f92e451
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-02-20 10:34:20 +0100

    use custom error to convert to `U32s`

--- twenty-first/src/error.rs
+++ twenty-first/src/error.rs
@@ -1,2 +1,11 @@
+use thiserror::Error;
+
 pub use crate::shared_math::bfield_codec::BFieldCodecError;
 pub use crate::util_types::merkle_tree::MerkleTreeError;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Error)]
+#[non_exhaustive]
+pub enum TryFromU32sError {
+    #[error("U32s<N>: `N` not big enough to hold the value")]
+    InsufficientSize,
+}
