b9264abb0b87665a192ccc02a47c1ab733011be3
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-04-26 21:00:30 +0200

    refactor!: remove `tree_m_ary.rs`

--- twenty-first/src/util_types.rs
+++ twenty-first/src/util_types.rs
@@ -3,4 +3,3 @@ pub mod merkle_tree;
 pub mod merkle_tree_maker;
 pub mod mmr;
 pub mod shared;
-pub mod tree_m_ary;
--- twenty-first/src/util_types/tree_m_ary.rs
+++
@@ -1,8 +0,0 @@
-use std::{cell::RefCell, rc::Rc};
-
-#[derive(Debug, Clone)]
-pub struct Node<T: Sized> {
-    pub children: Vec<Rc<RefCell<Node<T>>>>,
-
-    pub data: T,
-}
