290a2f8a2e7afa0972b1859a9e05d77bd1bbd817
Alan (olivas) <alan@neptune.cash>
2024-05-03 15:51:40 +0200

    bench: Add benchmark for polynomial modular reduction

---
+++ twenty-first/benches/poly_mod_reduce.rs
@@ -0,0 +1,32 @@
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::BenchmarkId;
+use criterion::Criterion;
+
+use twenty_first::math::other::random_elements;
+use twenty_first::prelude::*;
+
+criterion_main!(benches);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().sample_size(50);
+    targets = poly_mod_reduce<{ 1 << 14 }, {1<<4}>,
+        poly_mod_reduce<{ 1 << 14 }, {1<<6}>,
+        poly_mod_reduce<{ 1 << 14 }, {1<<8}>,
+);
+
+fn poly_mod_reduce<const SIZE_LHS: usize, const SIZE_RHS: usize>(c: &mut Criterion) {
+    let log2_of_size = SIZE_LHS.ilog2();
+    let mut group = c.benchmark_group(format!(
+        "Modular reduction of degree {} by degree {}",
+        SIZE_LHS - 1,
+        SIZE_RHS - 1
+    ));
+    let lhs = Polynomial::new(random_elements::<BFieldElement>(SIZE_LHS));
+    let rhs = Polynomial::new(random_elements::<BFieldElement>(SIZE_LHS));
+
+    let id = BenchmarkId::new("long division", log2_of_size);
+    group.bench_function(id, |b| b.iter(|| lhs.clone() % rhs.clone()));
+
+    group.finish();
+}
