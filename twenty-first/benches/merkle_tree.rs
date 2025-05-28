29c312f59cabfda04a32821b64d1efae28add25a
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-05-28 13:42:28 +0200

    bench: Bench all ways of computing a Merkle root

--- twenty-first/benches/merkle_tree.rs
+++ twenty-first/benches/merkle_tree.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use criterion::BenchmarkId;
 use criterion::Criterion;
 use criterion::criterion_group;
@@ -8,20 +6,35 @@ use twenty_first::math::digest::Digest;
 use twenty_first::math::other::random_elements;
 use twenty_first::util_types::merkle_tree::MerkleTree;
 
-fn merkle_tree(c: &mut Criterion) {
-    let mut group = c.benchmark_group("merkle_tree");
-
-    let exponent = 16;
-    let size = usize::pow(2, exponent);
-    group.sample_size(10);
-    group.measurement_time(Duration::from_secs(50));
-
-    let elements: Vec<Digest> = random_elements(size);
-
-    group.bench_function(BenchmarkId::new("merkle_tree", size), |bencher| {
-        bencher.iter(|| MerkleTree::par_new(&elements).unwrap());
-    });
-}
-
-criterion_group!(benches, merkle_tree);
 criterion_main!(benches);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = merkle_tree::<16>,
+              merkle_tree::<20>,
+);
+
+fn merkle_tree<const TREE_HEIGHT: usize>(c: &mut Criterion) {
+    let bench_id = BenchmarkId::new("height", TREE_HEIGHT);
+    let leafs: Vec<Digest> = random_elements(1 << TREE_HEIGHT);
+
+    c.benchmark_group("merkle_tree_parallel")
+        .bench_function(bench_id.clone(), |bencher| {
+            bencher.iter(|| MerkleTree::par_new(&leafs).unwrap());
+        });
+
+    c.benchmark_group("merkle_tree_sequential")
+        .bench_function(bench_id.clone(), |bencher| {
+            bencher.iter(|| MerkleTree::sequential_new(&leafs).unwrap());
+        });
+
+    c.benchmark_group("merkle_root_frugal_parallel")
+        .bench_function(bench_id.clone(), |bencher| {
+            bencher.iter(|| MerkleTree::par_frugal_root(&leafs).unwrap());
+        });
+
+    c.benchmark_group("merkle_root_frugal_sequential")
+        .bench_function(bench_id, |bencher| {
+            bencher.iter(|| MerkleTree::sequential_frugal_root(&leafs).unwrap());
+        });
+}
