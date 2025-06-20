22b03c8bcd8d0af8ecd7eb1f3f02cb47bb400fdf
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-06-20 13:03:12 +0200

    refactor(bench): Refactor Tip5 benchmark
    changelog: ignore

--- twenty-first/benches/tip5.rs
+++ twenty-first/benches/tip5.rs
@@ -4,74 +4,44 @@ use criterion::criterion_group;
 use criterion::criterion_main;
 use rand::random;
 use rayon::prelude::*;
+use std::time::Duration;
 use twenty_first::math::other::random_elements;
 use twenty_first::prelude::*;
 
-fn bench_10(c: &mut Criterion) {
-    let mut group = c.benchmark_group("tip5/hash_10");
-
-    let size = 10;
-    group.sample_size(100);
-
-    let single_element: [BFieldElement; 10] = random();
-    group.bench_function(BenchmarkId::new("Tip5 / Hash 10", size), |bencher| {
-        bencher.iter(|| Tip5::hash_10(&single_element));
-    });
-}
-
-fn bench_pair(c: &mut Criterion) {
-    let mut group = c.benchmark_group("tip5/hash_pair");
-
-    let left = random();
-    let right = random();
-
-    group.bench_function(BenchmarkId::new("Tip5 / Hash Pair", "pair"), |bencher| {
-        bencher.iter(|| Tip5::hash_pair(left, right));
-    });
-}
-
-fn bench_varlen(c: &mut Criterion) {
-    let mut group = c.benchmark_group("tip5/hash_varlen");
-
-    let many = 16_384;
-    let many_elements: Vec<BFieldElement> = random_elements(many);
-
-    group.sample_size(50);
-    group.bench_function(
-        BenchmarkId::new("Tip5 / Hash Variable Length", many),
-        |bencher| {
-            bencher.iter(|| Tip5::hash_varlen(&many_elements));
-        },
-    );
-
-    let few = 10;
-    let few_elements: Vec<BFieldElement> = random_elements(few);
-    group.bench_function(
-        BenchmarkId::new("Tip5 / Hash Variable Length", few),
-        |bencher| {
-            bencher.iter(|| Tip5::hash_varlen(&few_elements));
-        },
-    );
-}
-
-fn bench_parallel(c: &mut Criterion) {
-    let mut group = c.benchmark_group("tip5/parallel");
-
-    let size = 65536;
-    group.sample_size(50);
-    let elements: Vec<[BFieldElement; 10]> = (0..size)
-        .map(|_| random_elements(10).try_into().unwrap())
-        .collect();
-
-    group.bench_function(BenchmarkId::new("Tip5 / Parallel Hash", size), |bencher| {
-        bencher.iter(|| {
-            elements
-                .par_iter()
-                .map(Tip5::hash_10)
-                .collect::<Vec<[BFieldElement; Digest::LEN]>>()
-        });
-    });
-}
-
-criterion_group!(benches, bench_10, bench_pair, bench_varlen, bench_parallel);
 criterion_main!(benches);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(8));
+    targets =
+        hash_10,
+        hash_pair,
+        hash_varlen::<10>,
+        hash_varlen::<16_384>,
+        hash_parallel::<65_536>,
+);
+
+fn hash_10(c: &mut Criterion) {
+    let input = random();
+    c.bench_function("hash_10", |b| b.iter(|| Tip5::hash_10(&input)));
+}
+
+fn hash_pair(c: &mut Criterion) {
+    let (left, right) = random();
+    c.bench_function("hash_pair", |b| b.iter(|| Tip5::hash_pair(left, right)));
+}
+
+fn hash_varlen<const LEN: usize>(c: &mut Criterion) {
+    let input = random_elements(LEN);
+    c.benchmark_group("hash_varlen")
+        .bench_function(BenchmarkId::new("len", LEN), |b| {
+            b.iter(|| Tip5::hash_varlen(&input))
+        });
+}
+
+fn hash_parallel<const LEN: usize>(c: &mut Criterion) {
+    let input = (0..LEN).map(|_| random()).collect::<Vec<_>>();
+    c.benchmark_group("hash_parallel")
+        .bench_function(BenchmarkId::new("len", LEN), |b| {
+            b.iter(|| input.par_iter().map(Tip5::hash_10).collect::<Vec<_>>());
+        });
+}
