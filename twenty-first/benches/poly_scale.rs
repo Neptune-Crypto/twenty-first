e203ab242416e01a934be4185a3ce7a56e903929
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-10-20 13:46:37 +0200

    refactor!: Un-generalize some polynomial functions
    In particular, we have observed that `Polynomial::scale` is only used
    with an offset from the base field. The change cascades to both methods
    `Polynomial::fast_coset_{evaluate, interpolate}`, which are only used
    with a base field offset in downstream dependencies.
    
    This change paves the way for some memoization in `Polynomial::scale`.

--- twenty-first/benches/poly_scale.rs
+++ twenty-first/benches/poly_scale.rs
@@ -21,23 +21,13 @@ fn poly_scale<const LOG2_SIZE: usize>(c: &mut Criterion) {
 
     let bfe_poly = Polynomial::<BFieldElement>::new(random_elements((1 << LOG2_SIZE) + 1));
     let xfe_poly = Polynomial::<XFieldElement>::new(random_elements((1 << LOG2_SIZE) + 1));
-    let bfe_scalar: BFieldElement = random();
-    let xfe_scalar: XFieldElement = random();
+    let scalar = random();
 
-    group.bench_function(BenchmarkId::new("bfe poly, bfe scalar", LOG2_SIZE), |b| {
-        b.iter(|| bfe_poly.scale(bfe_scalar))
+    group.bench_function(BenchmarkId::new("bfe poly", LOG2_SIZE), |b| {
+        b.iter(|| bfe_poly.scale(scalar))
     });
-
-    group.bench_function(BenchmarkId::new("bfe poly, xfe scalar", LOG2_SIZE), |b| {
-        b.iter(|| bfe_poly.scale(xfe_scalar))
-    });
-
-    group.bench_function(BenchmarkId::new("xfe poly, bfe scalar", LOG2_SIZE), |b| {
-        b.iter(|| xfe_poly.scale(bfe_scalar))
-    });
-
-    group.bench_function(BenchmarkId::new("xfe poly, xfe scalar", LOG2_SIZE), |b| {
-        b.iter(|| xfe_poly.scale(xfe_scalar))
+    group.bench_function(BenchmarkId::new("xfe poly", LOG2_SIZE), |b| {
+        b.iter(|| xfe_poly.scale(scalar))
     });
 
     group.finish();
